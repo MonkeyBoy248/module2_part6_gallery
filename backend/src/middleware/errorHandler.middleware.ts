e814147d7486ce1475b3ab8b15efb1c0c4372d42
Kirill Temnyi <kirik201100@mail.ru>
2022-04-15 16:59:08 +0300

    refactor: change error logs message format
    replace error message with error name
    
    MODULE-2-PART-6

--- backend/src/middleware/errorHandler.middleware.ts
+++ backend/src/middleware/errorHandler.middleware.ts
@@ -2,7 +2,7 @@ import {NextFunction, Request, Response} from "express";
 import {writeLogs} from "../utils/writeLogs";
 
 export async function customErrorHandler (err: Error, req: Request, res: Response, next: NextFunction) {
-  await writeLogs(err.message);
+  await writeLogs(err.name);
 
-  res.status(500).send({error: 'Error has occurred'});
+  res.status(500).send({error: err.message});
 }
